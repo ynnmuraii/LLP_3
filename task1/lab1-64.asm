30d510c7145c323212f2740a54c3d0aba1d16356
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2024-11-21 03:36:12 +0400

    ofiget rabotaet malloc

--- task1/lab1-64.asm
+++ task1/lab1-64.asm
@@ -18,12 +18,18 @@ main:
     ; Пролог функции
     push rbp
     mov rbp, rsp
-    sub rsp, 32              ; Выравниваем стек для локальных переменных
+    sub rsp, 4+32              ; Выравниваем стек для локальных переменных
+    
+    mov rcx, 100
+    imul rcx, rcx, 4
+    call malloc
+    mov rdi, rax
 
     ; Считываем n
     lea rcx, [fmt]           ; 1-й аргумент в rcx
     lea rdx, [rbp-4]         ; 2-й аргумент в rdx
     call scanf
+
 
     mov ebx, [rbp-4]         ; n сохраняется в ebx
 
@@ -40,36 +46,36 @@ input_loop:
     jge sort_start 
 
     lea rcx, [fmt]
-    lea rdx, [arr + rsi*4]   ; Адрес текущего элемента массива
+    lea rdx, [rdi + rsi*4]   ; Адрес текущего элемента массива
     call scanf
 
     inc rsi
     jmp input_loop
 
 sort_start:
-    xor edi, edi             ; edi = 0 (left = 0)
+    xor r8d, r8d             ; edi = 0 (left = 0)
     mov esi, ebx             ; esi = n
     dec esi                  ; esi = n - 1 (right = n - 1)
 
 sorting_loop:
-    mov eax, edi             ; eax = left
+    mov eax, r8d             ; eax = left
     cmp eax, esi             ; left >= right?
     jge sort_end             ; Если да, завершить сортировку
 
-    mov ecx, edi             ; ecx = left
+    mov ecx, r8d             ; ecx = left
     
 first_loop_start:
     cmp ecx, esi             ; Пока ecx < right
     jge first_loop_end
 
-    mov edx, [arr + rcx*4]   ; edx = arr[ecx]
-    mov eax, [arr + rcx*4 + 4] ; eax = arr[ecx+1]
+    mov edx, [rdi + rcx*4]   ; edx = arr[ecx]
+    mov eax, [rdi + rcx*4 + 4] ; eax = arr[ecx+1]
     cmp edx, eax             ; Сравниваем arr[ecx] и arr[ecx+1]
     jle no_swap1
 
     ; Меняем местами arr[ecx] и arr[ecx+1]
-    mov [arr + rcx*4], eax
-    mov [arr + rcx*4 + 4], edx
+    mov [rdi + rcx*4], eax
+    mov [rdi + rcx*4 + 4], edx
 
 no_swap1:
     inc ecx                  ; ecx++
@@ -80,24 +86,24 @@ first_loop_end:
     mov ecx, esi             ; ecx = right
 
 second_loop_start:
-    cmp ecx, edi             ; Пока ecx > left
+    cmp ecx, r8d             ; Пока ecx > left
     jle second_loop_end
 
-    mov edx, [arr + rcx*4]   ; edx = arr[ecx]
-    mov eax, [arr + rcx*4 - 4] ; eax = arr[ecx-1]
+    mov edx, [rdi + rcx*4]   ; edx = arr[ecx]
+    mov eax, [rdi + rcx*4 - 4] ; eax = arr[ecx-1]
     cmp eax, edx             ; Сравниваем arr[ecx-1] и arr[ecx]
     jle no_swap2
 
     ; Меняем местами arr[ecx-1] и arr[ecx]
-    mov [arr + rcx*4 - 4], edx
-    mov [arr + rcx*4], eax
+    mov [rdi + rcx*4 - 4], edx
+    mov [rdi + rcx*4], eax
 
 no_swap2:
     dec ecx                  ; ecx--
     jmp second_loop_start
 
 second_loop_end:
-    inc edi                  ; left++
+    inc r8d                  ; left++
     jmp sorting_loop
 
 sort_end:
@@ -107,7 +113,7 @@ output_loop:
     cmp esi, ebx             ; Пока ecx < n
     jge end_program          ; Завершаем, если все элементы выведены
 
-    mov eax, [arr + rsi*4]   ; eax = arr[ecx]
+    mov eax, [rdi + rsi*4]   ; eax = arr[ecx]
     lea rcx, [fmt_out]       ; Адрес строки формата "%u "
     mov rdx, rax             ; Значение для вывода передается в rdx
     call printf              ; Выводим число
